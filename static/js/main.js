57d740c3adee5a8b994b1301def4cef4848ab31b
lilfireflylil <oofaisalahmadioo@gmail.com>
2024-05-12 18:59:34 +0430

    added the static file. css and js

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+function disappearRightSection() {
+    let element = document.getElementByClassName('page-header-right-section');
+    element.classList.add('page-header-right-section-disappear-when-loggedout')
+};
+
